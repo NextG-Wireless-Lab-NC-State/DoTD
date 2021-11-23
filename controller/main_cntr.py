d3e65dfa67ce688dbee9dfb3fcb00014a77b8eb9
dijkstra <mkassem.44t@gmail.com>
2021-11-23 18:06:40 +0000

    add controller file main_cntr.py

---
+++ controller/main_cntr.py
@@ -0,0 +1,113 @@
+import argparse
+import re
+import time
+import os
+import numpy as np
+import datetime
+
+import threading
+import Queue
+from copy import copy, deepcopy
+
+import networkx as nx
+import matplotlib.pyplot as plt
+import bellmanford as bf
+import itertools
+from multiprocessing import Process, Manager, Pool
+
+import sys
+sys.path.append("../")
+from mobility.read_real_tles import *
+from mobility.mobility_utils import *
+from mobility.read_gs import *
+from routing.routing_utils import *
+from comm_protocol.controller_main import *
+
+def extract_links(filename):
+    linksFile = open(filename, 'r')
+    lines = linksFile.readlines()
+    links = []
+    used_time = lines[0]
+
+    for i in range(1, len(lines)):
+        links.append(lines[i].strip())
+
+    year, month, day = used_time.split(" ")[0].split("-")
+    hour, min, sec = used_time.split(" ")[1].split(":")
+    min_num = 0
+    min_num += float(min) + (float(sec)/float(60.0))
+    ts = load.timescale()
+    t2 = ts.utc(int(year), int(month), int(day), int(hour), min_num)
+
+    return {"used_time" : t2,
+	    "links" : links
+	   }
+
+def extract_management_interfaces(filename):
+    mIntf_File = open(filename, 'r')
+    lines = mIntf_File.readlines()
+    m_intf = []
+
+    for i in range(len(lines)):
+	lines[i].split("\t")[0]
+        m_intf.append({"node": lines[i].split("\t")[0], "mgnt_ip": lines[i].split("\t")[1]})
+
+    return m_intf
+
+def get_management_ip(all_mgnt_ips, node):
+    for interface in all_mgnt_ips:
+	if interface["node"] == node:
+		return interface["mgnt_ip"]
+
+def main():
+    N = 3
+
+    satellites = load.tle_file("https://celestrak.com/NORAD/elements/starlink.txt")
+    satellites_by_name = {sat.name: sat for sat in satellites}
+    planes = extract_planes("../mobility/starlink_tles.txt")
+
+    cur_planes = planes["Planes"]
+    print len(planes["Unassigned"])
+
+    mininetExtract = extract_links("links_log.txt")
+    mininetExtract2 = extract_management_interfaces("m_intf_log.txt")
+
+    sorted_planes = sort_satellites_within_plane(cur_planes, satellites_by_name, mininetExtract["used_time"])
+
+    # Get the satellites in planes only, igonore the Unassigned satellites
+    available_satellites = []
+    for key in sorted_planes.keys():
+        sats = ""
+        for satellite in sorted_planes[key]:
+            sats += str(satellites_by_name[str(satellite)].name).split("-")[1]+","
+            available_satellites.append(satellites_by_name[str(satellite)])
+
+    available_satellites_by_name = {sat.name: sat for sat in available_satellites}
+    actual_sat_number_to_counter = label_satellites_properly(sorted_planes, len(available_satellites_by_name))
+
+    ground_stations = read_gs("../mobility/ground_stations.txt")
+
+    num_of_satellites = len(available_satellites_by_name)
+    num_of_ground_stations = len(ground_stations)
+
+    print num_of_satellites, num_of_ground_stations
+    conn_mat_size = num_of_satellites + num_of_ground_stations
+
+    #socket = establish_connection()
+    available_ips = generate_ips_for_constellation()
+    list_of_Intf_IPs = assign_ips_for_constellation(mininetExtract["links"], available_ips)
+    cnt = 0
+    for intf_ips in list_of_Intf_IPs:
+	print intf_ips
+	sat, interface = intf_ips["Interface"].split("-")
+	#socket = establish_connection()
+        ip_addr = get_management_ip(mininetExtract2, sat)
+	print sat
+	command_message = create_message("Assign IP", sat, "ifconfig", [intf_ips["Interface"], intf_ips["IP"]])
+	serverAddressPort=(str(ip_addr.strip()), 20001)
+	send_command(command_message, serverAddressPort)
+	time.sleep(0.01)
+	cnt+=1
+    print mininetExtract["used_time"]
+
+main()
